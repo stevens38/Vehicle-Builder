3611dd886b90b06df1929d6f7735f1c07f66e6c2
JStevens <stevens38@marshall.edu>
2024-10-12 18:42:31 -0400

    motorbike update

--- 02-Challenge/Develop/src/classes/Motorbike.ts
+++ 02-Challenge/Develop/src/classes/Motorbike.ts
@@ -3,24 +3,72 @@ import Vehicle from './Vehicle.js';
 import Wheel from './Wheel.js';
 
 // TODO: The Motorbike class should extend the Vehicle class
+// TODO: Declare properties of the Motorbike class
+// TODO: The properties should include vin, color, make, model, year, weight, top speed, and wheels
+// TODO: The types should be as follows: vin (string), color (string), make (string), model (string), year (number), weight (number), topSpeed (number), wheels (Wheel[])
+
 class Motorbike {
-  // TODO: Declare properties of the Motorbike class
-  // TODO: The properties should include vin, color, make, model, year, weight, top speed, and wheels
-  // TODO: The types should be as follows: vin (string), color (string), make (string), model (string), year (number), weight (number), topSpeed (number), wheels (Wheel[])
+  vin: string;
+  color: string;
+  make: string;
+  model: string;
+  year: number;
+  weight: number;
+  topSpeed: number;
+  wheels: Wheel[];
+
 
   // TODO: Create a constructor that accepts the properties of the Motorbike class
-    // TODO: The constructor should call the constructor of the parent class, Vehicle
-    // TODO: The constructor should initialize the properties of the Motorbike class
-    // TODO: The constructor should check if the wheels array has 2 elements and create 2 new default Wheel objects if it does not
+  // TODO: The constructor should call the constructor of the parent class, Vehicle
+  // TODO: The constructor should initialize the properties of the Motorbike class
+  // TODO: The constructor should check if the wheels array has 2 elements and create 2 new default Wheel objects if it does not
+  constructor(
+    vin: string,
+    color: string,
+    make: string,
+    model: string,
+    year: number,
+    weight: number,
+    topSpeed: number,
+    wheels: Wheel[],
+  ) {
 
-  // TODO: Implement the wheelie method
+    super();
+    this.vin = vin;
+    this.color = color;
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.weight = weight;
+    this.topSpeed = topSpeed;
+
+    if (wheels.length !== 2) {
+      this.wheels = [new Wheel(), new Wheel()];
+    } else {
+      this.wheels = wheels;
+    }
+    // TODO: Implement the wheelie method
     // TODO: The method should log the message "Motorbike [make] [model] is doing a wheelie!"
-
+    wheelie(): void {
+      console.log(`Motorbike ${this.make} ${this.model} is doing a wheelie!`);
+    }
   // TODO: Override the printDetails method from the Vehicle class
   // TODO: The method should call the printDetails method of the parent class
-  // TODO: The method should log the details of the Motorbike
-  // TODO: The details should include the VIN, make, model, year, weight, top speed, color, and wheels
-}
+    // TODO: The method should log the details of the Motorbike
+    // TODO: The details should include the VIN, make, model, year, weight, top speed, color, and wheels
+  override printDetails(): void {
+      super.printDetails();
+      console.log(`VIN: ${this.vin}`);
+      console.log(`Make: ${this.make}`);
+      console.log(`Model: ${this.model}`);
+      console.log(`Year: ${this.year}`);
+      console.log(`Weight: ${this.weight} lbs`);
+      console.log(`Top Speed: ${this.topSpeed} mph`);
+      console.log(`Color: ${this.color}`);
+      console.log(`Wheels: ${this.wheels.length}`);
+    }
+
+    
 
 // Export the Motorbike class as the default export
 export default Motorbike;
